2e9ab25881a5616a0a7955ee7e80794a738c9bfb
Ha3To <lgsc0409ld@gmail.com>
2025-03-08 01:46:03 +0900

    FakeUD_update

--- src/test_scripts/test_FakeUpdate_Flask.py
+++ src/test_scripts/test_FakeUpdate_Flask.py
@@ -30,4 +30,4 @@ def fake_malware():
     return send_file("malware.exe", as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=9090)
